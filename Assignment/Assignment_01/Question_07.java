4db9cafc274b7493e8c25c4546e8a4bf1660d066
Sakshi2736 <sakshianiljadhav@gmail.com>
2025-03-07 19:33:49 +0530

    Add files via upload

---
+++ Assignment/Assignment_01/Question_07.java
@@ -0,0 +1,8 @@
+class Question_07{
+public static void main(String agre[]){
+if(true){
+System.out.println("This will always run when it true");
+}
+else{
+System.out.println("This will when it false");
+}}}
