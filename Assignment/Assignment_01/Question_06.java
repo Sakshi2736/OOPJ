0eb9a88cc70f7d91223d2c15f2fd50a8f7e66fa6
Sakshi2736 <sakshianiljadhav@gmail.com>
2025-03-07 19:19:35 +0530

    delets

--- Assignment/Assignment_01/Question_06.java
+++
@@ -1,9 +0,0 @@
-class Question_06{
-public static void main(String arge[]){
-char ch = 'A';
-int i= ch; //assign A to i so it cover into int value i.e 65
-String str ="Hello";
-System.out.println("Character:" +ch); //print A
-System.out.println("ASCII Value:" +i); // print ASCII value of A =65 
-System.out.println("String:" +str); // print Hello
-}}
