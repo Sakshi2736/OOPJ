927714582c6751cf74066937c8e8726e0fbaad9f
Sakshi2736 <sakshianiljadhav@gmail.com>
2025-03-07 09:31:22 +0530

    Add files via upload

---
+++ Assignment/Question_09.java
@@ -0,0 +1,13 @@
+ class Question_09 {
+    public static void main(String[] args) {
+        int num1 = 10; // First predefined integer
+        int num2 = 5;  // Second predefined integer
+
+        System.out.println(num1 + " == " + num2 + ": " + (num1 == num2));
+        System.out.println(num1 + " != " + num2 + ": " + (num1 != num2));
+        System.out.println(num1 + " > " + num2 + ": " + (num1 > num2));
+        System.out.println(num1 + " < " + num2 + ": " + (num1 < num2));
+        System.out.println(num1 + " >= " + num2 + ": " + (num1 >= num2));
+        System.out.println(num1 + " <= " + num2 + ": " + (num1 <= num2));
+    }
+}
